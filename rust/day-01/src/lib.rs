69c17b6c9b1130d38eb5f4fab825a951c55324f9
juan felipe duarte <jf@duartte.co>
2022-12-04 00:52:49 +0100

    part 2

--- rust/day-01/src/lib.rs
+++ rust/day-01/src/lib.rs
@@ -12,13 +12,27 @@ pub fn process_part1(input: &str) -> String {
   result.to_string()
 }
 
+pub fn process_part2(input: &str) -> String {
+    let mut result = input
+        .split("\n\n")
+        .map(|elf_load| {
+            elf_load
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<_>>();
+
+    result.sort_by(|a, b| b.cmp(a));
+    let sum: u32 = result.iter().take(3).sum();
+    sum.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let input: &str = "1000
+    const INPUT: &str = "1000
 2000
 3000
 
@@ -32,7 +46,15 @@ mod tests {
 9000
 
 10000";
-      let result: String = process_part1(input);
+
+    #[test]
+    fn it_works() {
+      let result: String = process_part1(INPUT);
       assert_eq!(result, "24000");
+    }
+    #[test]
+    fn part2_works() {
+      let result: String = process_part1(INPUT);
+      assert_eq!(result, "45000");
     }
 }
